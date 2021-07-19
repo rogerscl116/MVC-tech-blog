f88a9ad389ddfc6b48272b6616ed4a44b56dff09
charity.rogers <rogerscl116@gmail.com>
2021-07-18 21:40:28 -0500

    add model associations and create api routes for the Post model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
