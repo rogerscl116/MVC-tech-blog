bbecc91ff13ea3dc5113cfc2f33137b53e28f4c5
Charity Rogers <rogerscl116@gmail.com>
2021-07-20 15:46:06 -0500

    add homepage

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+
+// redirect unauthenticated users attempting to access a page to the login page
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
