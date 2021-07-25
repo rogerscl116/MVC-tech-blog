892367356d0b5906307ade5180c9ed7520a1d8e2
charity.rogers <rogerscl116@gmail.com>
2021-07-24 22:06:53 -0500

    update user seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,49 +2,41 @@ const { User } = require('../models');
 
 const userData = [
     {
-      "id": 1,
       "username": "harleyquinn",
       "email": "harleyquinn@dc.com",
       "password": "puddin"
     },
     {
-      "id": 2,
       "username": "poisonivy",
       "email": "poisonivy@dc.com",
       "password": "pamela"
     },
     {
-      "id": 3,
       "username": "blackcanary",
       "email": "blackcanary@dc.com",
       "password": "dinah"
     },
     {
-      "id": 4,
       "username": "batwoman",
       "email": "batwoman@dc.com",
       "password": "kane"
     },
     {
-      "id": 5,
       "username": "huntress",
       "email": "huntress@dc.com",
       "password": "helena"
     },
     {
-      "id": 6,
       "username": "wonderwoman",
       "email": "wonderwoman@dc.com",
       "password": "amazons"
     },
     {
-      "id": 7,
       "username": "supergirl",
       "email": "supergirl@dc.com",
       "password": "krypton"
     },
     {
-      "id": 8,
       "username": "catwoman",
       "email": "catwoman@dc.com",
       "password": "selina"
